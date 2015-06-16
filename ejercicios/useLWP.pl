0abefb1c4943967e5b7a90205c29f3bee44e7589
Makova <makova65@gmail.com>
2015-06-16 23:52:32 +0200

    ejercicios de JJ

---
+++ ejercicios/useLWP.pl
@@ -0,0 +1,6 @@
+#!/usr/lib/perl
+
+# Create a user agent object
+      use LWP::UserAgent;
+      my $ua = LWP::UserAgent->new;
+      $ua->agent("MyApp/0.1 ");
---
+++ ejercicios/web.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+
+use LWP::Simple;
+
+getprint('https://localhost:8020/');
