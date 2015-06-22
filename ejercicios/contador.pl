281ebfebbf186c579911ff4cbe5ee4c6ef58df7a
Makova <makova65@gmail.com>
2015-06-23 00:17:16 +0200

    un par de ejercicios

---
+++ ejercicios/contador.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+ 
+  
+  my $contador=0;
+
+  
+  print "Se contar:", $contador++ , ", ", $contador ++,"...\n";
+
+  print "2 elevado a $contador es:", (2**$contador), "\n";
---
+++ ejercicios/otroArrayMas.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+my @primer_array;
+
+  
+@primer_array=(1,"dos",3,"cuatro");
+
+  
+$primer_array[4]=5.5;
+
+  
+print "El tercero es= ".$primer_array[3]." \n";
